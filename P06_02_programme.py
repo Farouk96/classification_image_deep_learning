04e544c1b1ee6853b495dd5653e4779ba19b19c0
Cecile Glt <cecile.gltslmcs@gmail.com>
2021-11-07 18:21:49 +0100

    add function for model importation

--- P06_02_programme.py
+++ P06_02_programme.py
@@ -1,14 +1,25 @@
 import gradio as gr
 import numpy as np
+import tensorflow as tf
+import cv2
 
-def sepia(input_img):
-  sepia_filter = np.array([[.393, .769, .189],
-                           [.349, .686, .168],
-                           [.272, .534, .131]])
-  sepia_img = input_img.dot(sepia_filter.T)
-  sepia_img /= sepia_img.max()                          
-  return sepia_img
 
-iface = gr.Interface(sepia, gr.inputs.Image(shape=(200, 200)), "image")
+model = tf.saved_model.load('my_model.h5')
+def image_classifier(im):
+  im = img_to_array(im)
+  im = cv2.resize(im,(224,224))
+  im = im.reshape(1,224,224,3)
+  predictions = model.predict(img)
+  score = tf.nn.softmax(predictions[0])
+  return {labels[i]: float(prediction[i]) for i in range(1000)}
 
-iface.launch()
+iface = gr.Interface(
+    image_classifier, 
+    gr.inputs.Image(shape=(224, 224)), 
+    gr.outputs.Label(score),
+    capture_session=True,
+    interpretation="default"
+    )
+
+if __name__ == "__main__":
+    iface.launch()
