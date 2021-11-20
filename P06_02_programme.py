c8d2c38a965d1c416554dcfc09ef1b670328e10d
Cecile Guillot <cecile.gltslmcs@gmail.com>
2021-11-20 21:45:34 +0100

    adding comments

--- P06_02_programme.py
+++ P06_02_programme.py
@@ -1,3 +1,4 @@
+# import libraries
 import streamlit as st
 import tensorflow as tf
 from tensorflow.keras.utils import img_to_array
@@ -6,18 +7,25 @@ from PIL import Image
 import cv2
 import numpy as np
 
+# Displaying in head
 st.title("Who let the dog out ?")
 st.header("Dog Breed Classification - Inception V3")
 st.image("banner.jpg")
 st.text("Upload a dog image to obtain its breed. Only 120 breeds available.")
 
-
+# Importing labels nome
 my_content = open("dogs_name.txt", "r")
 dog_names = my_content.read()
 dogs_list = dog_names.split('\n')
 my_content.close()
 
 def image_classifier(img, weights_file):
+  """Function which classifies an image. 
+
+  Inputs: Image to classify & Model to load
+
+  Output : Prediction with the greater probability
+  """
   model = keras.models.load_model(weights_file)
   image = img_to_array(img)
   image = cv2.resize(image,(224,224))
@@ -27,13 +35,15 @@ def image_classifier(img, weights_file):
   predictions = np.argmax(predictions)
   return dogs_list[predictions]
 
+# Displaying uploader
 uploaded_file = st.file_uploader("Upload your image...", type="jpg")
 
+# Loop ending with prediction
 if uploaded_file is not None:
   img = Image.open(uploaded_file)
   st.image(img, caption='Uploaded image.', use_column_width=True)
   st.write("")
-  st.write("Classifying...")
+  st.write("Classifying in progress...")
   label = image_classifier(img, 'my_model.h5')
   st.write(label)
 
